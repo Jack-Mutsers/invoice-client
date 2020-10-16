745b4a608cfef42292d5be876e85ed4092a2abee
Jack Mutsers <jmutsers95@hotmail.com>
2020-10-16 10:44:25 +0200

    added login message

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,10 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  loginStatus = "You are not logged in";
 
   constructor() { }
 
   ngOnInit(): void {
+    var userdata = JSON.parse(localStorage.getItem('currentUser'));
+
+    if(userdata){
+      this.loginStatus = "Welkom " + userdata.user.name;
+    }
   }
 
 }
