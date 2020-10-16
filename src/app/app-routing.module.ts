67882dd5648872af35f1cd3fb3730bbeb7786113
Jack Mutsers <jmutsers95@hotmail.com>
2020-10-16 10:31:08 +0200

    added home module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { CustomersComponent } from './customers/customers.component';
+import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { ProductcategoriesComponent } from './productcategories/productcategories.component';
 import { ProductsComponent } from './products/products.component';
@@ -12,7 +13,8 @@ const routes: Routes = [
   {path:'products', component: ProductsComponent},
   {path:'productcategories', component: ProductcategoriesComponent},
   {path:'users', component: UsersComponent},
-  {path:'login', component: LoginComponent}
+  {path:'login', component: LoginComponent},
+  {path:'', component: HomeComponent}
 ];
 
 @NgModule({
